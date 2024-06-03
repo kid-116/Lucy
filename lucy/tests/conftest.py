9d9275aaf72e85f31240bdf990576c6004392663
Mehul Todi <mehultodi116@gmail.com>
2024-06-03 13:13:46 +0530

    Set NThreads=1 for test

--- lucy/tests/conftest.py
+++ lucy/tests/conftest.py
@@ -1,24 +1,27 @@
 import shutil
-from typing import Generator
+from typing import Any, Generator
 
 from click.testing import CliRunner
 import pytest
 
+from lucy.main import config_set
 from lucy.config import TestConfig
 from lucy.filesystem import LocalFS
 
 
+@pytest.fixture(scope='session')
+def runner() -> CliRunner:
+    return CliRunner()
+
+
 @pytest.fixture(autouse=True, scope='session')
-def setup_env() -> Generator[None, None, None]:
+def setup_env(runner: CliRunner) -> Generator[None, None, None]:  # pylint: disable=redefined-outer-name
     # Setup.
+    args: list[Any] = ['NThreads', 1]
+    runner.invoke(config_set, args)
     LocalFS.setup()
 
     yield
 
     # Tear-down.
     shutil.rmtree(TestConfig.home)
-
-
-@pytest.fixture(scope='session')
-def runner() -> CliRunner:
-    return CliRunner()
