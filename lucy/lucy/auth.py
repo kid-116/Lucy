2af93f68746cd5cb9f112c0e429b49bf335dab47
Mehul Todi <mehultodi116@gmail.com>
2024-06-03 22:45:23 +0530

    Check if login is successful

--- lucy/lucy/auth.py
+++ lucy/lucy/auth.py
@@ -28,6 +28,8 @@ class Auth:  # pylint: disable=too-few-public-methods
         scraper.driver.find_element(value='username').send_keys(website_config.user_id)
         scraper.driver.find_element(value='password').send_keys(website_config.passwd)
         scraper.driver.find_element(value='submit').click()
+        if scraper.driver.current_url != f'{website_config.host}/home':
+            raise ValueError('Invalid credentials.')
         token_cookie = scraper.driver.get_cookie(website_config.auth_token_name)
         assert token_cookie
         token = Token.encode(token_cookie['value'])
